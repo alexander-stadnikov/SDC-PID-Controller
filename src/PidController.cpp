36186e6247d17fd285068e4e0aa1ef47d765b743
Alexander <alexander.stadnikov@gmail.com>
2021-03-15 09:21:18 +0100

    Add configuration methods to Throttle

--- src/PidController.cpp
+++ src/PidController.cpp
@@ -26,27 +26,63 @@ namespace sdc
         return -m_p * m_error.p - m_i * m_error.i * m_i - m_d * m_error.d;
     }
 
+    Throttle::Throttle() noexcept
+        : m_throttle(0.0),
+          m_maxThrottle(0.0),
+          m_maxBrake(0.0),
+          m_maxCteThreshold(0.0),
+          m_minCteThreshold(0.0),
+          m_stabilizationSpeed(0.0)
+    {
+    }
+
+    void Throttle::SetMaxThrottle(double v) noexcept
+    {
+        m_maxThrottle = v;
+    }
+
+    void Throttle::SetMaxBrake(double v) noexcept
+    {
+        m_maxBrake = v;
+    }
+
+    void Throttle::SetMaxCteThreshold(double v) noexcept
+    {
+        m_maxCteThreshold = v;
+    }
+
+    void Throttle::SetMinCteThreshold(double v) noexcept
+    {
+        m_minCteThreshold = v;
+    }
+
+    void Throttle::SetStabilizationSpeed(double v) noexcept
+    {
+        m_stabilizationSpeed = v;
+    }
+
     double Throttle::Value() const noexcept
     {
-
         return m_throttle;
     }
 
     void Throttle::Update(double cte, double speed) noexcept
     {
-        m_throttle = 1.0;
-        if (std::fabs(cte) > .8)
+        m_throttle = m_maxThrottle;
+        cte = std::fabs(cte);
+
+        if (cte > m_maxCteThreshold)
         {
             m_slowDown = true;
         }
 
         if (m_slowDown)
         {
-            if (speed > 50)
+            if (speed > m_stabilizationSpeed)
             {
-                m_throttle = -1.0;
+                m_throttle = m_maxBrake;
             }
-            else if (std::fabs(cte) <= 0.3)
+            else if (cte <= m_minCteThreshold)
             {
                 m_slowDown = false;
             }
--- src/PidController.h
+++ src/PidController.h
@@ -5,12 +5,25 @@ namespace sdc
     class Throttle
     {
     public:
+        Throttle() noexcept;
+
+        void SetMaxThrottle(double) noexcept;
+        void SetMaxBrake(double) noexcept;
+        void SetMaxCteThreshold(double) noexcept;
+        void SetMinCteThreshold(double) noexcept;
+        void SetStabilizationSpeed(double) noexcept;
+
         void Update(double cte, double speed) noexcept;
         double Value() const noexcept;
 
     private:
         bool m_slowDown = false;
         double m_throttle;
+        double m_maxThrottle;
+        double m_maxBrake;
+        double m_maxCteThreshold;
+        double m_minCteThreshold;
+        double m_stabilizationSpeed;
     };
 
     class PidController final
