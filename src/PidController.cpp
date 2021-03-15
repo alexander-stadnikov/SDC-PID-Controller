4f1555c59d7ed4fef8cd2e843798820834e61e05
Alexander <alexander.stadnikov@gmail.com>
2021-03-15 09:09:08 +0100

    Add Throttle Controller

--- src/PidController.cpp
+++ src/PidController.cpp
@@ -1,5 +1,7 @@
 #include "PidController.h"
 
+#include <cmath>
+
 namespace sdc
 {
     void PidController::Init(double p, double i, double d) noexcept
@@ -19,8 +21,35 @@ namespace sdc
         m_prevCte = cte;
     }
 
-    double PidController::TotalError() noexcept
+    double PidController::TotalError() const noexcept
     {
         return -m_p * m_error.p - m_i * m_error.i * m_i - m_d * m_error.d;
+    }
+
+    double Throttle::Value() const noexcept
+    {
+
+        return m_throttle;
+    }
+
+    void Throttle::Update(double cte, double speed) noexcept
+    {
+        m_throttle = 1.0;
+        if (std::fabs(cte) > .8)
+        {
+            m_slowDown = true;
+        }
+
+        if (m_slowDown)
+        {
+            if (speed > 50)
+            {
+                m_throttle = -1.0;
+            }
+            else if (std::fabs(cte) <= 0.3)
+            {
+                m_slowDown = false;
+            }
+        }
     }
 }
--- src/PidController.h
+++ src/PidController.h
@@ -2,12 +2,23 @@
 
 namespace sdc
 {
+    class Throttle
+    {
+    public:
+        void Update(double cte, double speed) noexcept;
+        double Value() const noexcept;
+
+    private:
+        bool m_slowDown = false;
+        double m_throttle;
+    };
+
     class PidController final
     {
     public:
         void Init(double p, double i, double d) noexcept;
         void UpdateError(double cte) noexcept;
-        double TotalError() noexcept;
+        double TotalError() const noexcept;
 
     private:
         struct Error
