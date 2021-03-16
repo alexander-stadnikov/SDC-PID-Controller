8c2b4e665d8eb215b22d6bdaf37f90f39e89b770
Alexander <alexander.stadnikov@gmail.com>
2021-03-16 09:47:04 +0100

    Use Twiddler, PidController and Throttle in project

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,29 @@
 int main()
 {
     uWS::Hub h;
+    constexpr bool useTwiddler = false;
+    const int StepsInEpoch = 500;
+    int steps = 0;
 
+    const sdc::PidController::Tau initialTau = useTwiddler
+                                                   ? sdc::PidController::Tau{0.15, 0.0004, 3}
+                                                   : sdc::PidController::Tau{0.16, 0.0003, 3};
     sdc::PidController pid;
-    pid.Init(0.0, 0.0, 0.0);
+    pid.Init(initialTau);
+    pid.SetTotalErrorNormalizer(StepsInEpoch / 2);
 
     sdc::Throttle throttle;
     throttle.SetMaxThrottle(1.0);
     throttle.SetMaxBrake(-1.0);
-    throttle.SetMaxCteThreshold(0.8);
+    throttle.SetMaxCteThreshold(.7);
     throttle.SetMinCteThreshold(0.3);
-    throttle.SetStabilizationSpeed(30.0);
+    throttle.SetStabilizationSpeed(45.0);
 
-    sdc::Twiddler twiddler;
+    sdc::Twiddler twiddler(initialTau, 0.1);
 
-    h.onMessage([&pid, &throttle, &twiddler](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                                             uWS::OpCode opCode) {
+    h.onMessage([&pid, &throttle, &twiddler, &steps](uWS::WebSocket<uWS::SERVER> ws,
+                                                     char *data, size_t length,
+                                                     uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
@@ -47,27 +55,42 @@ int main()
                     double speed = std::stod(j[1]["speed"].get<std::string>());
                     // double angle = std::stod(j[1]["steering_angle"].get<std::string>());
 
-                    pid.UpdateError(cte);
-                    throttle.Update(cte, speed);
-                    double steer_value = pid.GetSteering();
-                    twiddler.Update();
-
-                    if (twiddler.IsEpochDone())
+                    if (useTwiddler && steps >= StepsInEpoch)
                     {
+                        twiddler.Update(pid.GetTotalError());
+                        pid.Init(twiddler.GetTau());
+                        steps = 0;
+
                         std::string reset_msg = "42[\"reset\",{}]";
                         ws.send(reset_msg.data(), reset_msg.length(), uWS::OpCode::TEXT);
+
+                        const auto t = twiddler.GetTau();
+                        std::cout << "p=" << t.p
+                                  << " i=" << t.i
+                                  << " d=" << t.d
+                                  << " e=" << twiddler.GetBestError()
+                                  << " s=" << twiddler.GetSumOfIncrements()
+                                  << std::endl;
+
                         return;
                     }
 
+                    pid.UpdateError(cte);
+                    throttle.Update(cte, speed);
+                    double steer_value = pid.GetSteering();
+                    steer_value = std::fmax(-1.0, steer_value);
+                    steer_value = std::fmin(1.0, steer_value);
+                    steps++;
+
                     // DEBUG
-                    std::cout << "CTE: " << cte << " Steering Value: " << steer_value
-                              << std::endl;
+                    // std::cout << "CTE: " << cte << " Steering Value: " << steer_value
+                    //           << std::endl;
 
                     nlohmann::json msgJson;
                     msgJson["steering_angle"] = steer_value;
                     msgJson["throttle"] = throttle.GetValue();
                     auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-                    std::cout << msg << std::endl;
+                    //std::cout << msg << std::endl;
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
                 } // end "telemetry" if
             }
